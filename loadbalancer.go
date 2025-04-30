918c3d1dfd8883b836202d032f4e113a4d6e63a8
szeyu <szeyusim@gmail.com>
2025-04-30 23:11:30 +0800

    Add mutex for thread safety in LoadBalancer struct and update GetNextApiKey method to use locking

--- loadbalancer.go
+++ loadbalancer.go
@@ -1,9 +1,14 @@
 package main
 
+import (
+	"sync"
+)
+
 // create a load balancer object
 type LoadBalancer struct {
-	apiKeys []string // list of api keys
-	index   int      // index of the current api key
+	apiKeys []string     // list of api keys
+	index   int          // index of the current api key
+	mutex   sync.RWMutex // mutex for thread safety
 }
 
 // constructor for the load balancer
@@ -12,12 +17,17 @@ func NewLoadBalancer(apiKeys []string) LoadBalancer {
 	return LoadBalancer{
 		apiKeys: apiKeys,
 		index:   0, // initalize the first api key
+		mutex:   sync.RWMutex{},
 	}
 }
 
 // get the next api key
 // lb is a LoadBalancer object alias
 func (lb *LoadBalancer) GetNextApiKey() string {
+	// Lock for writing
+	lb.mutex.Lock()
+	defer lb.mutex.Unlock()
+
 	apiKey := lb.apiKeys[lb.index] // get the current api key
 	lb.index = (lb.index + 1) % len(lb.apiKeys)
 
